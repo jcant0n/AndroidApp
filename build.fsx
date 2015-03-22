0500eda6dcae57e03dc060e9d6b39dd7ccc0d54f
Javier Canton Ferrero <javiercantonferrero@gmail.com>
2015-03-22 02:16:17 +0100

    Added directly upload to HockeyApp

--- build.fsx
+++ build.fsx
@@ -1,5 +1,7 @@
 #r @"packages/FAKE.3.5.4/tools/FakeLib.dll"
 #load "build-helpers.fsx"
+#load "hockey-app-helpers.fsx"
+
 open Fake
 open Fake
 open System
@@ -7,6 +9,7 @@ open System.IO
 open System.Linq
 open BuildHelpers
 open Fake.XamarinHelper
+open HockeyAppHelper
 
 Target "android-build" (fun () ->
  MSBuild "AndroidApp/bin/Release" "Build" [ ("Configuration", "Release"); ("Platform", "Any CPU") ] [ "AndroidApp.sln" ] |> ignore
@@ -22,7 +25,19 @@ Target "android-package" (fun () ->
     |> fun file -> TeamCityHelper.PublishArtifact file.FullName
 )
 
+Target "android-deploy" (fun () ->
+ Environment.SetEnvironmentVariable("HockeyAppApiToken", "ba597cb15f28485db212aa8a4d10037d")
+ 
+ let buildCounter = BuildHelpers.GetBuildCounter TeamCityHelper.TeamCityBuildNumber
+ 
+ let hockeyAppApiToken = Environment.GetEnvironmentVariable("HockeyAppApiToken")
+ 
+ let appPath = "AndroidApp/bin/Release/AndroidApp.AndroidApp.apk"
+ 
+ HockeyAppHelper.Upload hockeyAppApiToken appPath buildCounter
+)
+
 "android-build"
   ==> "android-package"
-  
+
 RunTarget() 
